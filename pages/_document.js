2675fa7543dc22321161bb8b8a00c2ed3bee1b6a
Tylarcam <tylarcam@alumni.stanford.edu>
2022-12-19 13:13:59 -0800

    link_fix

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ export default function Document() {
         <meta property="og:description" content="build with buildspace" key="description"/>
         <meta
           property="og:image"
-          content="https://cdn.buildspace.so/courses/gpt3-writer/project-og.jpg"
+          content="https://images.squarespace-cdn.com/content/v1/5cc114bdfb22a5152e1c3431/6152b80c-e910-496f-8044-0f045af21a43/DSC_5817.JPG?format=1500w.jpg"
         />
         <meta name="twitter:card" content="summary_large_image"></meta>
       </Head>
